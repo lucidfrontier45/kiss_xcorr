bbea6c9b89dfe0b98dc0ce41f1547d97a8d0205b
Shiqiao Du <s.du@freebit.net>
2013-12-19 11:35:55 +0900

    added include guard

--- include/kiss_xcorr.h
+++ include/kiss_xcorr.h
@@ -5,6 +5,13 @@
  *      Author: du
  */
 
+#ifndef KISS_XCORR_H
+#define KISS_XCORR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdlib.h>
 #include "kiss_fftndr.h"
 
@@ -52,3 +59,9 @@ void rfft_xcorrnd(size_t freq_len, const kiss_fft_scalar *x,
  */
 void rfft_xcorrnd2(const int *dims, int ndims, const kiss_fft_scalar *x,
 		const kiss_fft_scalar *y, kiss_fft_scalar *z, int mode);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#endif /* KISS_XCORR_H */
